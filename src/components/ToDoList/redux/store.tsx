6a6b445a01d6e0c4d358fda7f28062b349a84413
Ilya frontend <zaxarov.ilya.00@inbox.ru>
2024-07-10 23:19:34 +0500

    fix error

--- src/components/ToDoList/redux/store.tsx
+++ src/components/ToDoList/redux/store.tsx
@@ -1,7 +1,8 @@
 import { combineReducers, compose, legacy_createStore } from "redux";
 import { serviceReducer } from "./serviceReducer";
 
-const ReactReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const ReactReduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+
 
 export function configureStore() {
   return legacy_createStore(
